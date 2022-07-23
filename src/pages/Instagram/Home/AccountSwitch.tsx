2f0f70d43689249242b939beaa294bfd1785fda8
rashedrahat <imrashedrahat@gmail.com>
2022-07-23 22:44:49 +0600

    Design Account Switch section

---
+++ src/pages/Instagram/Home/AccountSwitch.tsx
@@ -0,0 +1,47 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import appRoutes from "../../../utils/routes";
+import Image from "../../../components/ui/Image";
+
+type User = {
+  username: string;
+  name: string;
+  profilePicture: string;
+};
+
+type AccountSwitchProps = {
+  currUser: User;
+};
+
+function AccountSwitch({ currUser }: AccountSwitchProps) {
+  const { username, name, profilePicture } = currUser;
+  return (
+    <div className="flex items-center justify-between">
+      <div className="flex items-center gap-x-4">
+        <Image
+          srcURL={profilePicture}
+          alt="Profile Picture"
+          className="rounded-full w-14 h-14 object-cover"
+        />
+        <div className="flex flex-col items-start">
+          <p className="text-base font-semibold text-instagram-primary">
+            {username}
+          </p>
+          <p className="text-base font-semibold text-instagram-secondary">
+            {name}
+          </p>
+        </div>
+      </div>
+      <div>
+        <Link
+          to={appRoutes.INSTAGRAM_HOME}
+          className="text-sm text-instagram-blue font-semibold cursor-not-allowed"
+        >
+          Switch
+        </Link>
+      </div>
+    </div>
+  );
+}
+
+export default AccountSwitch;
--- src/pages/Instagram/Home/Index.tsx
+++ src/pages/Instagram/Home/Index.tsx
@@ -3,6 +3,7 @@ import React from "react";
 import InstagramHeader from "../../../layouts/InstagramHeader";
 import Stories from "./Stories";
 import * as data from "../../../data/instagram.json";
+import AccountSwitch from "./AccountSwitch";
 
 function Home() {
   const { user, stories } = data;
@@ -11,12 +12,12 @@ function Home() {
     <div style={{ backgroundColor: "#FAFAFA" }}>
       <InstagramHeader profilePicture={user.profilePicture} />
       <div className="container mx-auto md:py-5 mt-14 md:mt-16">
-        <div className="flex justify-center gap-x-5">
+        <div className="flex justify-center md:gap-x-5 lg:gap-x-10">
           <div className="h-screen w-full md:w-1/2">
             <Stories currUserAvatar={user.profilePicture} data={stories} />
           </div>
           <div className="hidden md:flex flex-col h-screen w-1/2">
-            <div>1</div>
+            <AccountSwitch currUser={user} />
             <div>2</div>
           </div>
         </div>
