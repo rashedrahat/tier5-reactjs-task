0229ee8f48a5b7e013321f4b3bfc67200e8ed852
rashedrahat <imrashedrahat@gmail.com>
2022-07-25 03:36:29 +0600

    Design 404 page

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -1,7 +1,20 @@
 import React from "react";
+import { TbError404 } from "react-icons/tb";
+import { Link } from "react-router-dom";
+import appRoutes from "../utils/routes";
 
 function NotFound() {
-  return <div>NotFound</div>;
+  return (
+    <div className="flex flex-col items-center justify-center h-screen">
+      <TbError404 size={64} />
+      <Link
+        to={appRoutes.LANDING}
+        className="bg-gradient-to-r from-blue-300 to-blue-600 hover:from-pink-500 hover:to-yellow-500 text-base font-normal rounded-full p-3"
+      >
+        Take me Home
+      </Link>
+    </div>
+  );
 }
 
 export default NotFound;
