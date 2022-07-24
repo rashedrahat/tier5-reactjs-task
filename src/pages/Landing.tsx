5fed7474553c42c9a930ad1a093495e7e0abd3fc
rashedrahat <imrashedrahat@gmail.com>
2022-07-25 03:48:55 +0600

    Update design of Landing page

--- src/pages/Landing.tsx
+++ src/pages/Landing.tsx
@@ -5,16 +5,25 @@ import appRoutes from "../utils/routes";
 
 function Landing() {
   return (
-    <div className="flex items-center justify-center h-screen gap-x-5">
-      <Link to={appRoutes.FACEBOOK_HOME}>
-        <FaFacebook
-          size={48}
-          className="cursor-pointer fill-facebook-primary"
-        />
-      </Link>
-      <Link to={appRoutes.INSTAGRAM_HOME}>
-        <FaInstagram size={48} className="cursor-pointer" />
-      </Link>
+    <div className="flex items-center justify-center h-screen gap-x-7">
+      <div className="flex flex-col gap-y-2 items-center">
+        <FaFacebook className="fill-facebook-primary w-10 h-10 md:w-12 md:h-12" />
+        <Link
+          to={appRoutes.FACEBOOK_HOME}
+          className="text-facebook-primary text-sm text-normal"
+        >
+          Go to Facebook
+        </Link>
+      </div>
+      <div className="flex flex-col gap-y-2 items-center">
+        <FaInstagram className="w-10 h-10 md:w-12 md:h-12" />
+        <Link
+          to={appRoutes.INSTAGRAM_HOME}
+          className="text-instagram-primary text-sm text-normal"
+        >
+          Go to Instagram
+        </Link>
+      </div>
     </div>
   );
 }
