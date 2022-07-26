e1d00eef08a7049073bc4b4e7eaa16c83c9cfcca
rashedrahat <imrashedrahat@gmail.com>
2022-07-26 15:58:30 +0600

    Update design of active users section

--- src/pages/Facebook/Home/ActiveUsers.tsx
+++ src/pages/Facebook/Home/ActiveUsers.tsx
@@ -8,7 +8,7 @@ type ActiveUsersProps = {
 
 function ActiveUsers({ data }: ActiveUsersProps) {
   return (
-    <div className="bg-white max-w-screen-md overflow-x-auto online-scrollbar h-auto rounded-lg mx-3 md:mx-5 lg:mx-0 shadow flex md:hidden items-center justify-around p-4 gap-x-3">
+    <div className="bg-white max-w-full overflow-x-auto online-scrollbar h-22 rounded-lg mx-3 md:mx-5 lg:mx-0 shadow flex md:hidden items-center p-2 gap-x-2">
       {data?.map((user) => {
         return (
           <ActiveUser name={user.name} profilePicture={user.profilePicture} />
